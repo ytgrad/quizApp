d819a39a13cfb830ff1980aa88b2053bded5af07
ytgrad <ytgrad17@gmail.com>
2024-03-29 07:34:36 +0530

    checking answers, moving to next q, and stopping at finish

--- app/src/main/java/com/example/quizzapp/QuestionsActivity.kt
+++ app/src/main/java/com/example/quizzapp/QuestionsActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.quizzapp.databinding.ActivityQuestionsBinding
 
@@ -27,6 +28,7 @@ class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
         binding.tvOption2.setOnClickListener(this)
         binding.tvOption3.setOnClickListener(this)
         binding.tvOption4.setOnClickListener(this)
+        binding.btnSubmit.setOnClickListener(this)
 
     }
 
@@ -39,6 +41,9 @@ class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
     }
     private fun defaultOptionView(){  // sets all the options to default style
         val options = ArrayList<TextView>()
+        if(currentPosition < questionsList!!.size){
+            binding.btnSubmit.text = "S U B M I T"
+        }
         options.add(0, binding.tvOption1)
         options.add(1, binding.tvOption2)
         options.add(2, binding.tvOption3)
@@ -60,12 +65,47 @@ class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
         binding.tvOption3.text = question.optionThree
         binding.tvOption4.text = question.optionFour
     }
+
+    private fun setOptionsColor(clickedOption: Int, drawable: Int){
+        when(clickedOption){
+            1 -> binding.tvOption1.background = ContextCompat.getDrawable(this, drawable)
+            4 -> binding.tvOption4.background = ContextCompat.getDrawable(this, drawable)
+            3 -> binding.tvOption3.background = ContextCompat.getDrawable(this, drawable)
+            2 -> binding.tvOption2.background = ContextCompat.getDrawable(this, drawable)
+        }
+    }
     override fun onClick(view: View?) {
         when(view?.id){
             R.id.tvOption1 -> selectedOptionView(binding.tvOption1, 1)
             R.id.tvOption2 -> selectedOptionView(binding.tvOption2, 2)
             R.id.tvOption3 -> selectedOptionView(binding.tvOption3, 3)
             R.id.tvOption4 -> selectedOptionView(binding.tvOption4, 4)
+            R.id.btnSubmit -> {
+                    if(selectedOption == 0){
+                        defaultOptionView()
+                        currentPosition++
+                        when{
+                            currentPosition <= questionsList!!.size -> {
+                                    selectQuestion()
+                            }
+                            else -> {
+                                Toast.makeText(this, "finished", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }else{
+                        val question = questionsList?.get(currentPosition - 1)
+                        if(question!!.correctAns != selectedOption){
+                            setOptionsColor(selectedOption, R.drawable.rectangle_incorrect)
+                        }
+                        setOptionsColor(question.correctAns, R.drawable.rectangle_correct)
+                        selectedOption = 0
+                        if(currentPosition == questionsList!!.size){
+                            binding.btnSubmit.text = "F I N I S H"
+                        }else{
+                            binding.btnSubmit.text = "N E X T"
+                        }
+                    }
+            }
         }
     }
 
